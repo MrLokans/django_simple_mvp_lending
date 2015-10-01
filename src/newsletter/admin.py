5af8110d111adb006d4b0ae70adbe19380251398
MrLokans <trikster1911@gmail.com>
2015-10-01 16:09:04 +0300

    Model look changed.

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import SignUp
 # Register your models here.
+# https://docs.djangoproject.com/en/1.8/ref/contrib/admin/
 
-admin.site.register(SignUp)
+
+class SignUpAdmin(admin.ModelAdmin):
+    # This is how we change the way model is displayed in the admin menu
+    list_display = ["__unicode__", "timestamp", "updated"]
+
+    class Meta:
+        model = SignUp
+
+
+admin.site.register(SignUp, SignUpAdmin)
