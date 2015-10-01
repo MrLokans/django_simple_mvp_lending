9763337fbb1efc3d9ffca15db80e456996a92deb
MrLokans <trikster1911@gmail.com>
2015-10-01 15:59:25 +0300

    Model added to admin site.

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Newsletter(models.Model):
+# https://docs.djangoproject.com/en/1.8/ref/models/fields/
+
+
+class SignUp(models.Model):
     email = models.EmailField()
-    full_name = models.CharField(120)
-    timestamp = models.DateTimeField()
+    full_name = models.CharField(max_length=120, default='John Doe', blank=True, null=True)
+    # auto add timestamp on create
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    # represented in admin site
+    def __unicode__(self): # __str__  for python 3
+        return self.email
