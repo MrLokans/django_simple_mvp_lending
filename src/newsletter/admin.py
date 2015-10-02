b2b645a6f642fef937f80690e6138259f95a563f
MrLokans <trikster1911@gmail.com>
2015-10-02 14:30:40 +0300

    Admin form changed to manually created form

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .forms import SignUpForm
 from .models import SignUp
 # Register your models here.
 # https://docs.djangoproject.com/en/1.8/ref/contrib/admin/
@@ -9,8 +10,9 @@ class SignUpAdmin(admin.ModelAdmin):
     # This is how we change the way model is displayed in the admin menu
     list_display = ["__unicode__", "timestamp", "updated"]
 
-    class Meta:
-        model = SignUp
+    form = SignUpForm
+    # class Meta:
+    #     model = SignUp
 
 
 admin.site.register(SignUp, SignUpAdmin)
