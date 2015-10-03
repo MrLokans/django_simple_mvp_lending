f82a6523824ac5fa5a516bddcbdfa8039ce99e0b
MrLokans <trikster1911@gmail.com>
2015-10-03 11:40:58 +0300

    Advanced forms.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     title = "Hello."
     # Note, you may use request and auth objects right in the template
     # due to context_processors option in TEMPLATES section of settings.py
-    if request.user.is_authenticated():
-        title = "Hello, {}".format(request.user)
+    # if request.user.is_authenticated():
+    #     title = "Hello, {}".format(request.user)
+    # print request
+    # if request.method == "POST":
+    #     print request.POST
+    form = SignUpForm(request.POST or None)
+
+    if form.is_valid():
+        form_instance = form.save(commit=False)
+    if not form_instance.full_name:
+        form_instance.full_name = "John Galt"
+    form_instance.save()
     context = {
         "title": title,
         "user": request.user,
+        "form": form,
     }
 
     return render(request, "home.html", context)
